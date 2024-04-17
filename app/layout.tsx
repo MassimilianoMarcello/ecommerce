947b17f5751cb9ad30914943de8c28f194db8b0b
massimiliano <massimarcello@hotmail.it>
2024-04-17 17:09:21 +0200

    j

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {" "}
       <body className={inter.className}>
-        <NavBar/>
+        <NavBar />
         {children}
-        
-        </body>
+      </body>
     </html>
   );
 }
